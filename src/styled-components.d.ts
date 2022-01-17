24b4303393e0398245217db64c1eb40bcc7416d2
Beatrice Guerra <beatrice.guerra@zextras.com>
2022-01-17 18:29:49 +0100

    chore: add missing license header

--- src/styled-components.d.ts
+++ src/styled-components.d.ts
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { ThemeObj } from './theme/theme';
 
 declare module 'styled-components' {
