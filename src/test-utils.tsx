8b8240fd323c7681bcfd746edcaec3dc8e729952
Beatrice Guerra <beatrice.guerra@zextras.com>
2022-02-08 17:19:04 +0100

    refactor: move test-utils to typescript and update fonts type

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -3,14 +3,13 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-/* eslint-disable import/no-extraneous-dependencies */
 
 import React from 'react';
-import { render as rtlRender } from '@testing-library/react';
+import { render as rtlRender, RenderResult } from '@testing-library/react';
 import { ThemeProvider } from './theme/theme-context-provider';
 
-export function render(ui, { ...options } = {}) {
-	const Wrapper = ({ children }) => <ThemeProvider>{children}</ThemeProvider>;
+export function render(ui: React.ReactElement, { ...options } = {}): RenderResult {
+	const Wrapper: React.FC = ({ children }) => <ThemeProvider>{children}</ThemeProvider>;
 
 	return rtlRender(ui, {
 		wrapper: Wrapper,
