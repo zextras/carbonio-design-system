e4da2f3b7fc759d72a8f6afe7c6b0f76014f9345
Cataldo Mazzilli <cm@zextras.com>
2022-03-01 11:28:44 +0100

    refactor(tabBar): move to typescript
    refs: CDS-23

--- src/components/navigation/TabBar.md
+++ src/components/navigation/TabBar.md
@@ -89,25 +89,82 @@ const [selected, setSelected] = useState('tab-one');
   </Container>
 </>
 ```
+
+### forceWidthEquallyDistributed prop
+```jsx
+import {useState} from 'react';
+import {Container, Divider, Text} from '@zextras/carbonio-design-system';
+const items = [
+  { id: 'tab-one', label: 'looooooooooooooooooooooooooooooong label' },
+  { id: 'tab-two', label: 'Second Tab' },
+  { id: 'tab-three', label: 'Tab 3' }
+];
+const [selected, setSelected] = useState('tab-one');
+
+<>
+  <TabBar
+    items={items}
+	selected={selected}
+	onChange={console.log}
+	onItemClick={(ev) => setSelected(ev.selectedItemId)}
+    width={512}
+    height={48}
+  />
+  <TabBar
+   	items={items}
+	selected={selected}
+	onChange={console.log}
+	onItemClick={(ev) => setSelected(ev.selectedItemId)}
+	width={512}
+	height={48}
+    forceWidthEquallyDistributed
+  />
+  <Container
+    background="gray4"
+    width={512}
+    padding={{ all: 'small'}}
+    crossAlignment="flex-start"
+  >
+    <Text size="large">
+	  {`Selected: '${selected}'`}
+	</Text>
+  </Container>
+</>
+```
+
 ### Customized TabBar
 ```jsx
 import {useState} from 'react';
 import {Container, Divider, Text, DefaultTabBarItem, Icon} from '@zextras/carbonio-design-system';
+import styled, { css } from 'styled-components';
+
+const CustomContainer = styled(Container)`
+  min-width: 0;
+  flex-basis: fit-content;
+	${({ forceWidthEquallyDistributed }) =>
+	forceWidthEquallyDistributed &&
+	css`
+      flex-basis: unset;
+	`};
+  flex-grow: 1;
+`
+
 const CustomComponent = ({
     item,
     index,
     selected,
     onClick,
 }) => (
-  <Container onClick={onClick} width={'100%'} background={selected ? 'highlight' : 'gray4'}>
+  <CustomContainer onClick={onClick} background={selected ? 'highlight' : 'gray4'}>
     <Text size="large" color={selected ? 'primary' : 'error'}>{item.label}</Text>
-  </Container>
+  </CustomContainer>
 );
 const ReusedDefaultTabBar = ({
     item,
     index,
     selected,
     onClick,
+    forceWidthEquallyDistributed
 }) => (
   <DefaultTabBarItem
     item={item}
@@ -115,8 +172,9 @@ const ReusedDefaultTabBar = ({
     selected={selected}
     onClick={onClick}
     orientation="horizontal"
+	forceWidthEquallyDistributed={forceWidthEquallyDistributed}
   >
-    <Icon size="large" icon={item.icon}/>
+	{item.icon && <Icon size="large" icon={item.icon}/>}
     <Text size="large">{item.label}</Text>
   </DefaultTabBarItem>
 );
@@ -156,17 +214,30 @@ const [click, setClick] = useState('');
 ```jsx
 import {useState} from 'react';
 import {Container, Divider, Text, Icon} from '@zextras/carbonio-design-system';
+import styled, { css } from 'styled-components';
+
+const CustomContainer = styled(Container)`
+  min-width: 0;
+  flex-basis: fit-content;
+	${({ forceWidthEquallyDistributed }) =>
+	forceWidthEquallyDistributed &&
+	css`
+      flex-basis: unset;
+	`};
+  flex-grow: 1;
+`
+
 const CustomComponent = ({
     item,
     index,
     selected,
     onClick,
 }) => (
-  <Container onClick={onClick} width="100%" height={50} background={selected ? 'highlight' : 'gray4'}>
+  <CustomContainer onClick={onClick} height={50} background={selected ? 'highlight' : 'gray4'}>
     <Text size="large" color={selected ? 'primary' : 'error'}>{item.label}</Text>
     {item.specialProp && (<Text size="large" color={selected ? 'primary' : 'text'}>{item.specialProp}</Text>)}
     {item.icon && (<Icon icon={item.icon} size="large" color={selected ? 'primary' : 'info'}/>)}
-  </Container>
+  </CustomContainer>
 );
 const items = [
   { id: 'one', label: 'Hello' },
--- src/components/navigation/TabBar.tsx
+++ src/components/navigation/TabBar.tsx
@@ -4,15 +4,14 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import React, { useState, useEffect, useCallback, useMemo, useRef } from 'react';
-import styled, { css } from 'styled-components';
+import React, { useState, useEffect, useCallback, useMemo, useRef, HTMLAttributes } from 'react';
+import styled, { css, FlattenSimpleInterpolation } from 'styled-components';
 import { map } from 'lodash';
-import PropTypes from 'prop-types';
 import Container from '../layout/Container';
 import Text from '../basic/Text';
 import { useKeyboard, getKeyboardPreset } from '../../hooks/useKeyboard';
 import { useCombinedRefs } from '../../hooks/useCombinedRefs';
-import { Theme } from '../../theme/theme';
+import { ThemeObj } from '../../theme/theme';
 
 const CustomText = styled(Text)`
 	line-height: 1.5;
@@ -22,7 +21,7 @@ const DefaultTabBarItemContainer = styled(Container)`
 	outline: none;
 	min-width: 0;
 	flex-basis: fit-content;
-	${({ forceWidthEquallyDistributed }) =>
+	${({ forceWidthEquallyDistributed }): FlattenSimpleInterpolation =>
 		forceWidthEquallyDistributed &&
 		css`
 			flex-basis: unset;
@@ -30,7 +29,7 @@ const DefaultTabBarItemContainer = styled(Container)`
 	flex-grow: 1;
 	height: 100%;
 	transition: 0.2s ease-out;
-	border-bottom: ${({ theme, selected, underlineColor }) =>
+	border-bottom: ${({ theme, selected, underlineColor }): string =>
 		selected
 			? `1px solid ${theme.palette[underlineColor || 'primary'].regular}`
 			: '1px solid transparent'};
@@ -38,27 +37,65 @@ const DefaultTabBarItemContainer = styled(Container)`
 	user-select: none;
 
 	&:hover {
-		background: ${({ theme, background, disabled }) =>
+		background: ${({ theme, background, disabled }): string =>
 			background
 				? theme.palette[background][disabled ? 'disabled' : 'hover']
 				: theme.palette.transparent[disabled ? 'regular' : 'hover']};
 	}
 	&:focus {
-		background: ${({ theme, background, disabled }) =>
+		background: ${({ theme, background, disabled }): string =>
 			background
 				? theme.palette[background][disabled ? 'disabled' : 'focus']
 				: theme.palette.transparent[disabled ? 'regular' : 'focus']};
 	}
 `;
 
-const DefaultTabBarItem = React.forwardRef(function DefaultTabBarItemFn(
+interface Item {
+	id: string;
+	label: string | React.ReactElement;
+	CustomComponent?: React.ComponentType<DefaultTabBarItemProps & HTMLAttributes<HTMLDivElement>>;
+	disabled?: boolean;
+}
+
+interface TabBarProps {
+	/** List of elements, can have extra attributes to pass down to the CustomComponent */
+	items: Array<Item>;
+	/** id of the selected item */
+	selected?: string;
+	/** id of the default selected item */
+	defaultSelected?: string;
+	/** change callback, is called with the new selected id */
+	onChange: (selectedId: string) => void;
+	/** click (or also keyboard in the default component) event,
+	 * the selectedItemId field is added to the event object */
+	onItemClick: (ev: React.SyntheticEvent & { selectedItemId: string }) => void;
+	/** background color of the tabBar */
+	background: string | keyof ThemeObj['palette'];
+	/** underline color of the selected tab */
+	underlineColor: string | keyof ThemeObj['palette'];
+	forceWidthEquallyDistributed: boolean;
+}
+
+interface DefaultTabBarItemProps {
+	item: Item;
+	selected: boolean;
+	background: string | keyof ThemeObj['palette'];
+	onClick: React.ReactEventHandler;
+	underlineColor: string | keyof ThemeObj['palette'];
+	forceWidthEquallyDistributed: boolean;
+}
+
+const DefaultTabBarItem = React.forwardRef<
+	HTMLElement,
+	DefaultTabBarItemProps & HTMLAttributes<HTMLDivElement>
+>(function DefaultTabBarItemFn(
 	{
 		item,
 		selected,
 		background,
 		onClick,
 		underlineColor,
-		forceWidthEquallyDistributed,
+		forceWidthEquallyDistributed = false,
 		children,
 		...rest
 	},
@@ -71,8 +108,8 @@ const DefaultTabBarItem = React.forwardRef(function DefaultTabBarItemFn(
 		[item.disabled, onClick]
 	);
 
-	const innerRef = useRef();
-	const combinedRef = useCombinedRefs(ref, innerRef);
+	const innerRef = useRef<HTMLElement>(null);
+	const combinedRef = useCombinedRefs<HTMLElement>(ref, innerRef);
 
 	const keyEvents = useMemo(() => getKeyboardPreset('button', activationCb), [activationCb]);
 	useKeyboard(combinedRef, keyEvents);
@@ -104,7 +141,7 @@ const DefaultTabBarItem = React.forwardRef(function DefaultTabBarItemFn(
 	);
 });
 
-const TabBar = React.forwardRef(function TabBarFn(
+const TabBar = React.forwardRef<unknown, TabBarProps>(function TabBarFn(
 	{
 		items,
 		selected,
@@ -112,8 +149,8 @@ const TabBar = React.forwardRef(function TabBarFn(
 		onChange,
 		onItemClick,
 		background,
-		underlineColor,
-		forceWidthEquallyDistributed,
+		underlineColor = 'primary',
+		forceWidthEquallyDistributed = false,
 		...rest
 	},
 	ref
@@ -126,11 +163,12 @@ const TabBar = React.forwardRef(function TabBarFn(
 		}
 	}, [selected, onChange]);
 	const onItemClickCb = useCallback(
-		(id) => (ev) => {
-			setCurrentSelection(id);
-			onChange(id);
-			onItemClick({ ...ev, selectedItemId: id });
-		},
+		(id: string) =>
+			(ev: React.SyntheticEvent): void => {
+				setCurrentSelection(id);
+				onChange(id);
+				onItemClick({ ...ev, selectedItemId: id });
+			},
 		[onChange, onItemClick]
 	);
 	return (
@@ -141,15 +179,14 @@ const TabBar = React.forwardRef(function TabBarFn(
 			mainAlignment="flex-start"
 			{...rest}
 		>
-			{map(items, (item, index) =>
+			{map(items, (item) =>
 				item.CustomComponent ? (
 					<item.CustomComponent
 						key={item.id}
 						item={item}
-						index={index}
 						selected={item.id === currentSelection}
 						onClick={onItemClickCb(item.id)}
-						tabIndex={item.disabled ? null : 0}
+						tabIndex={item.disabled ? undefined : 0}
 						background={background}
 						underlineColor={underlineColor}
 						forceWidthEquallyDistributed={forceWidthEquallyDistributed}
@@ -158,11 +195,10 @@ const TabBar = React.forwardRef(function TabBarFn(
 					<DefaultTabBarItem
 						key={item.id}
 						item={item}
-						index={index}
 						selected={item.id === currentSelection}
 						background={background}
 						onClick={onItemClickCb(item.id)}
-						tabIndex={item.disabled ? null : 0}
+						tabIndex={item.disabled ? undefined : 0}
 						underlineColor={underlineColor}
 						forceWidthEquallyDistributed={forceWidthEquallyDistributed}
 					/>
@@ -171,45 +207,5 @@ const TabBar = React.forwardRef(function TabBarFn(
 		</Container>
 	);
 });
-
-TabBar.propTypes = {
-	/** List of elements, can have extra attributes to pass down to the CustomComponent */
-	items: PropTypes.arrayOf(
-		PropTypes.shape({
-			id: PropTypes.string.isRequired,
-			label: PropTypes.oneOfType([PropTypes.string, PropTypes.elementType]).isRequired,
-			CustomComponent: PropTypes.elementType,
-			disabled: PropTypes.bool
-		})
-	).isRequired,
-	/** id of the selected item */
-	selected: PropTypes.string,
-	/** id of the default selected item */
-	defaultSelected: PropTypes.string,
-	/** change callback, is called with the new selected id */
-	onChange: PropTypes.func,
-	/** click (or also keyboard in the default component) event,
-	 * the selectedItemId field is added to the event object */
-	onItemClick: PropTypes.func,
-	/** background color of the tabBar */
-	background: PropTypes.oneOfType([PropTypes.string, PropTypes.oneOf(Object.keys(Theme.palette))]),
-	/** underline color of the selected tab */
-	underlineColor: PropTypes.oneOfType([
-		PropTypes.string,
-		PropTypes.oneOf(Object.keys(Theme.palette))
-	]),
-	/** tabbar Width Equally Distributed beetwen tabs */
-	forceWidthEquallyDistributed: PropTypes.bool
-};
-
-TabBar.defaultProps = {
-	underlineColor: 'primary',
-	selected: undefined,
-	defaultSelected: undefined,
-	onChange: undefined,
-	onItemClick: undefined,
-	background: undefined,
-	forceWidthEquallyDistributed: false
-};
 
 export { TabBar, DefaultTabBarItem };
