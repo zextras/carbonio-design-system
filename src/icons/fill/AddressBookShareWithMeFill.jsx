450e62653477c1e362463ab71e7c045e58bc0454
Matteo Dal Zovo <zovomat@gmail.com>
2022-03-14 11:58:37 +0100

    feat: updated icon set with new icons

---
+++ src/icons/fill/AddressBookShareWithMeFill.jsx
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import * as React from 'react';
+
+function SvgAddressBookShareWithMeFill(props) {
+	return (
+		<svg width={24} height={24} xmlns="http://www.w3.org/2000/svg" {...props}>
+			<path
+				fillRule="evenodd"
+				d="M19 21H7c-1.6 0-3-1.4-3-3v-1H3c-.5 0-1-.5-1-1 0-.6.5-1 1-1h1V9H3c-.5 0-1-.5-1-1 0-.6.5-1 1-1h1V6c0-1.6 1.4-3 3-3h12c1.6 0 3 1.4 3 3v12c0 1.6-1.4 3-3 3zm-2.7-9.7h-4l.9-1c.1-.2.2-.4.2-.6 0-.2-.1-.4-.2-.5-.1-.1-.3-.2-.5-.2s-.4.1-.5.2l-2 2.3-.1.1v.1c-.1.1-.1.2-.1.2v.1c0 .1 0 .2.1.3 0 .1 0 .2.1.2l2.1 2.3c.1 0 .1.1.2.1.1.1.2.1.3.1.1 0 .2 0 .3-.1.1 0 .1-.1.2-.1.1-.1.1-.2.2-.3v-.3-.2c-.1-.1-.1-.2-.2-.3l-.9-.9h3.9c.2 0 .4-.1.5-.3.1-.1.2-.3.2-.5s-.1-.4-.2-.5c-.1-.2-.3-.2-.5-.2z"
+			/>
+		</svg>
+	);
+}
+
+export default SvgAddressBookShareWithMeFill;
---
+++ src/icons/outline/CalendarShareWithMeOutline.jsx
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import * as React from 'react';
+
+function SvgCalendarShareWithMeOutline(props) {
+	return (
+		<svg width={24} height={24} xmlns="http://www.w3.org/2000/svg" {...props}>
+			<path d="M18 4h-1V3a1 1 0 00-2 0v1H9V3a1 1 0 00-2 0v1H6a3 3 0 00-3 3v12a3 3 0 003 3h12a3 3 0 003-3V7a3 3 0 00-3-3zM6 6h1v1a1 1 0 002 0V6h6v1a1 1 0 002 0V6h1a1 1 0 011 1v6H5V7a1 1 0 011-1zm12 14H6a1 1 0 01-1-1v-6h14v6a1 1 0 01-1 1zm-2.7-7.748h-3.962l.875-.983a.782.782 0 00.193-.537.777.777 0 00-.218-.525.676.676 0 00-.5-.207.68.68 0 00-.49.233l-2.002 2.251a.758.758 0 00-.063.098.55.55 0 00-.077.142.701.701 0 00-.042.21L9 13.002a.797.797 0 00.203.525l2.1 2.251a.7.7 0 00.228.164.66.66 0 00.766-.164.796.796 0 000-1.066l-.91-.96H15.3a.677.677 0 00.495-.22.779.779 0 00.205-.53.779.779 0 00-.205-.53.677.677 0 00-.495-.22z" />
+		</svg>
+	);
+}
+
+export default SvgCalendarShareWithMeOutline;
