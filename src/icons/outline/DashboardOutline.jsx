fea626b3e805485487ac0bb04f6e35c63ec7d511
Matteo Dal Zovo <matteo.dalzovo@zextras.com>
2022-04-20 15:29:32 +0200

    feat: new icons

---
+++ src/icons/outline/DashboardOutline.jsx
@@ -0,0 +1,63 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import * as React from 'react';
+
+function SvgDashboardOutline(props) {
+	return (
+		<svg width={24} height={24} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M19.943 20.016H4.057a9.938 9.938 0 01-2.041-6.047c0-5.51 4.473-9.985 9.984-9.985 5.51 0 9.984 4.474 9.984 9.985 0 2.272-.76 4.368-2.04 6.047zm-1.052-1.953H5.109a7.973 7.973 0 01-1.125-4.094A8.02 8.02 0 0112 5.953a8.02 8.02 0 016.891 12.11z"
+				fill="#414141"
+			/>
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M16.7 10.25a1 1 0 00-1.414 0l-4.142 4.143a1 1 0 001.415 1.414l4.142-4.142a1 1 0 000-1.414z"
+				fill="#414141"
+			/>
+			<path d="M13.253 10.19a.995.995 0 100-1.99.995.995 0 000 1.99z" fill="#414141" />
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M13.253 8.2a.995.995 0 11-.001 1.99.995.995 0 010-1.99zm0 1.973a.978.978 0 110-1.957.978.978 0 010 1.957z"
+				fill="#414141"
+			/>
+			<path d="M17.026 14.795a.995.995 0 100-1.99.995.995 0 000 1.99z" fill="#414141" />
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M17.026 12.806a.995.995 0 110 1.99.995.995 0 010-1.99zm0 1.972a.978.978 0 11.001-1.956.978.978 0 01-.001 1.956z"
+				fill="#414141"
+			/>
+			<path d="M6.971 14.992a.995.995 0 100-1.99.995.995 0 000 1.99z" fill="#414141" />
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M6.971 13.002a.995.995 0 110 1.99.995.995 0 010-1.99zm0 1.973a.978.978 0 11.001-1.957.978.978 0 010 1.957z"
+				fill="#414141"
+			/>
+			<path d="M10.253 10.31a.995.995 0 100-1.99.995.995 0 000 1.99z" fill="#414141" />
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M10.253 8.32a.995.995 0 11-.001 1.99.995.995 0 010-1.99zm0 1.973a.978.978 0 110-1.957.978.978 0 010 1.957z"
+				fill="#414141"
+			/>
+			<path d="M7.839 12.179a.995.995 0 100-1.99.995.995 0 000 1.99z" fill="#414141" />
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M7.839 10.19a.995.995 0 11-.001 1.99.995.995 0 010-1.99zm0 1.972a.978.978 0 110-1.956.978.978 0 010 1.956z"
+				fill="#414141"
+			/>
+		</svg>
+	);
+}
+
+export default SvgDashboardOutline;
