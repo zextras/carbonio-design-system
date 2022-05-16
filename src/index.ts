57ec0448830ee823325742666dd764d0e02371cd
Beatrice Guerra <beatrice.guerra@zextras.com>
2022-05-16 12:23:47 +0200

    build: update build types script

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-/* eslint-disable import/no-cycle */
 /** Basic components */
 export * from './components/basic/Avatar';
 export * from './components/basic/Badge';
