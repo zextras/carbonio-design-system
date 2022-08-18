13769e01fccbf07a6f9b879d5c540ac316e829c8
Beatrice Guerra <beatrice.guerra@zextras.com>
2022-08-18 11:07:35 +0200

    feat(useSplitVisibility): add optional max number of visibile items
    Change args to accept an option object with
    - removeFrom: from where items should be removed to be placed on hidden list
    - maxVisible: max number of visible items that should be set even if available with could accept more
    
    Keep also previous type signature for compatibility
    
    refs: CDS-38

--- src/hooks/useSplitVisibility.tsx
+++ src/hooks/useSplitVisibility.tsx
@@ -4,12 +4,18 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { drop, head, last, slice } from 'lodash';
+
+type Options = {
+	removeFrom: 'end' | 'start';
+	maxVisible?: number;
+};
 
 function useSplitVisibility<T, R extends HTMLElement = HTMLElement>(
 	items: T[],
-	removeFrom = 'end'
+	// TODO(BREAKING CHANGE): remove string types and keep only Option type and rename to "options"
+	optionsWithRetroCompatibility: Options | 'end' | 'start' = { removeFrom: 'end' }
 ): [T[], T[], React.RefObject<R>] {
 	const [visibleItems, setVisibleItems] = useState<T[]>(items);
 	const [hiddenItems, setHiddenItems] = useState<T[]>([]);
@@ -17,51 +23,68 @@ function useSplitVisibility<T, R extends HTMLElement = HTMLElement>(
 	const [width, setWidth] = useState(window.innerWidth);
 	const [lastHiddenWidth, setLastHiddenWidth] = useState(0);
 
+	// TODO(BREAKING CHANGE): remove when options will be only an Option type
+	const options = useMemo<Options>(
+		() =>
+			typeof optionsWithRetroCompatibility === 'string'
+				? {
+						removeFrom: optionsWithRetroCompatibility
+				  }
+				: optionsWithRetroCompatibility,
+		[optionsWithRetroCompatibility]
+	);
+
 	useEffect(() => {
-		setVisibleItems(items);
-		setHiddenItems([]);
+		if (typeof options.maxVisible === 'number') {
+			setVisibleItems(items.slice(0, options.maxVisible));
+			setHiddenItems(items.slice(options.maxVisible));
+		} else {
+			setVisibleItems(items);
+			setHiddenItems([]);
+		}
 		setLastHiddenWidth(0);
-	}, [items]);
+	}, [items, options.maxVisible]);
 
 	useEffect(() => {
 		function handleResize(): void {
 			setWidth(window.innerWidth);
 		}
 		window.addEventListener('resize', handleResize);
-		return () => window.removeEventListener('resize', handleResize);
+		return (): void => {
+			window.removeEventListener('resize', handleResize);
+		};
 	}, []);
 
 	const containerRef = useRef<R>(null);
 
 	useLayoutEffect(() => {
-		if (
-			containerRef.current &&
-			containerRef.current.offsetWidth >= lastHiddenWidth &&
-			hiddenItems.length > 0
-		) {
-			if (removeFrom === 'end') {
-				setVisibleItems([...visibleItems, head(hiddenItems) as T]);
-				setHiddenItems(drop(hiddenItems));
-			} else {
-				setVisibleItems([last(hiddenItems) as T, ...visibleItems]);
-				setHiddenItems(slice(hiddenItems, 0, hiddenItems.length - 1));
+		if (containerRef?.current) {
+			const { offsetWidth, scrollWidth } = containerRef.current;
+			if (
+				offsetWidth >= lastHiddenWidth &&
+				hiddenItems.length > 0 &&
+				(typeof options.maxVisible !== 'number' || visibleItems.length < options.maxVisible)
+			) {
+				if (options.removeFrom === 'end') {
+					setVisibleItems([...visibleItems, head(hiddenItems) as T]);
+					setHiddenItems(drop(hiddenItems));
+				} else {
+					setVisibleItems([last(hiddenItems) as T, ...visibleItems]);
+					setHiddenItems(slice(hiddenItems, 0, hiddenItems.length - 1));
+				}
+			}
+			if (scrollWidth > offsetWidth && visibleItems.length > 0) {
+				if (options.removeFrom === 'end') {
+					setHiddenItems([last(visibleItems) as T, ...hiddenItems]);
+					setVisibleItems(slice(visibleItems, 0, visibleItems.length - 1));
+				} else {
+					setHiddenItems([...hiddenItems, head(visibleItems) as T]);
+					setVisibleItems(drop(visibleItems));
+				}
+				setLastHiddenWidth(scrollWidth);
 			}
 		}
-		if (
-			containerRef.current &&
-			containerRef.current.scrollWidth > containerRef.current.offsetWidth &&
-			visibleItems.length > 0
-		) {
-			if (removeFrom === 'end') {
-				setHiddenItems([last(visibleItems) as T, ...hiddenItems]);
-				setVisibleItems(slice(visibleItems, 0, visibleItems.length - 1));
-			} else {
-				setHiddenItems([...hiddenItems, head(visibleItems) as T]);
-				setVisibleItems(drop(visibleItems));
-			}
-			setLastHiddenWidth(containerRef.current.scrollWidth);
-		}
-	}, [width, items, lastHiddenWidth, hiddenItems, visibleItems, removeFrom]);
+	}, [width, items, lastHiddenWidth, hiddenItems, visibleItems, options]);
 
 	return [visibleItems, hiddenItems, containerRef];
 }
