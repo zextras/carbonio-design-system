f08c195e4f04ecae452c92c9dff41e8a56979b5f
Beatrice Guerra <beatrice.guerra@zextras.com>
2023-01-12 12:49:18 +0100

    build(deps): reset rollup to v2
    Rollup v3 has some breaking changes which require a more in depth investigation
    
    Use @rollup/plugin-url instead of rollup-plugin-url
    
    refs: CDS-110

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -7,7 +7,7 @@
 import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import url from 'rollup-plugin-url';
+import url from '@rollup/plugin-url';
 
 const plugins = [
 	url({
