592176b8c0487f32367ec9e6a17fb5665d744dd7
Beatrice Guerra <beatrice.guerra@zextras.com>
2023-04-18 16:42:30 +0200

    feat(Container): allow set of single borders through an object

---
+++ src/components/layout/Container.test.tsx
@@ -0,0 +1,44 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+import 'jest-styled-components';
+
+import { render } from '../../test-utils';
+import { Theme } from '../../theme/theme';
+import { Container } from './Container';
+
+describe('Container', () => {
+	test('Set all borders in one if a string is passed as prop', () => {
+		render(<Container borderColor={'black'}>Test container</Container>);
+		const containerEl = screen.getByText('Test container');
+		expect(containerEl).toHaveStyleRule(
+			'border',
+			expect.stringContaining(Theme.palette.black.regular)
+		);
+		expect(containerEl).not.toHaveStyleRule('border-top');
+		expect(containerEl).not.toHaveStyleRule('border-right');
+		expect(containerEl).not.toHaveStyleRule('border-bottom');
+		expect(containerEl).not.toHaveStyleRule('border-left');
+	});
+
+	test('Set only provided borders if an object is passed as prop', () => {
+		render(<Container borderColor={{ top: 'black', right: 'black' }}>Test container</Container>);
+		const containerEl = screen.getByText('Test container');
+		expect(containerEl).toHaveStyleRule(
+			'border-top',
+			expect.stringContaining(Theme.palette.black.regular)
+		);
+		expect(containerEl).toHaveStyleRule(
+			'border-right',
+			expect.stringContaining(Theme.palette.black.regular)
+		);
+		expect(containerEl).not.toHaveStyleRule('border-bottom');
+		expect(containerEl).not.toHaveStyleRule('border-left');
+		expect(containerEl).not.toHaveStyleRule('border');
+	});
+});
