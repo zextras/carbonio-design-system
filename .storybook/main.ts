fec0d2cdb6858a1598b8a466706875c80c03e35b
rodleyorosa <rodley.orosa@zextras.com>
2024-03-25 11:42:50 +0100

    refactor: remove unnecessary addons
    Refs: CDS-214

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,15 +3,15 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: [
-		'@storybook/addon-webpack5-compiler-swc',
-		'@storybook/addon-onboarding',
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@chromatic-com/storybook',
-		'@storybook/addon-interactions',
-		'@storybook/addon-themes',
-		'@storybook/test'
-	],
+        '@storybook/addon-webpack5-compiler-babel',
+        '@storybook/addon-essentials',
+        '@storybook/addon-themes',
+        '@storybook/addon-interactions',
+        '@storybook/test',
+        '@storybook/blocks',
+        '@storybook/react',
+        '@storybook/react-webpack5'
+    ],
 	framework: {
 		name: '@storybook/react-webpack5',
 		options: {}
