3875a61ade348ac52c91d663136a16452e8b5282
rodleyorosa <rodley.orosa@zextras.com>
2024-04-08 15:10:11 +0200

    feat: remove useless play functions in Banner stories
    Refs: CDS-210

--- src/components/feedback/banner/Banner.stories.tsx
+++ src/components/feedback/banner/Banner.stories.tsx
@@ -7,11 +7,9 @@
 import React from 'react';
 
 import type { Meta, StoryObj } from '@storybook/react';
-import { expect, within } from '@storybook/test';
 import { noop } from 'lodash';
 
 import { Banner } from './Banner';
-import { ICONS } from '../../../testUtils/constants';
 import { ModalManager } from '../../utilities/ModalManager';
 
 const meta: Meta = {
@@ -42,11 +40,6 @@ export const Description: Story = {
 		closeLabel: 'Close label',
 		showClose: false,
 		onClose: noop
-	},
-	play: async ({ canvasElement }) => {
-		const canvas = within(canvasElement);
-		await expect(canvas.getByTestId(ICONS.successBannerIcon)).toBeVisible();
-		await expect(canvas.getByText(/description is required/i)).toBeVisible();
 	}
 };
 
@@ -55,12 +48,6 @@ export const Title: Story = {
 		...Default.args,
 		title: ' Title is optional',
 		severity: 'warning'
-	},
-	play: async ({ canvasElement }) => {
-		const canvas = within(canvasElement);
-		await expect(canvas.getByTestId(ICONS.warningBannerIcon)).toBeVisible();
-		await expect(canvas.getByText(/description is required/i)).toBeVisible();
-		await expect(canvas.getByText(/title is optional/i)).toBeVisible();
 	}
 };
 
@@ -70,17 +57,9 @@ export const CloseBanner: Story = {
 		closeLabel: 'This is the close label',
 		showClose: true,
 		onClose: noop,
-		primaryAction: { label: 'Primary action', onClick: console.log },
-		secondaryAction: { label: 'Secondary action', onClick: console.log },
+		primaryAction: { label: 'Primary action', onClick: noop },
+		secondaryAction: { label: 'Secondary action', onClick: noop },
 		severity: 'info'
-	},
-	play: async ({ canvasElement }) => {
-		const canvas = within(canvasElement);
-		await expect(canvas.getByTestId(ICONS.infoBannerIcon)).toBeVisible();
-		await expect(canvas.getByText(/description is required/i)).toBeVisible();
-		await expect(canvas.getByRole('button', { name: /primary action/i })).toBeVisible();
-		await expect(canvas.getByRole('button', { name: /secondary action/i })).toBeVisible();
-		await expect(canvas.getByTestId(ICONS.close)).toBeVisible();
 	}
 };
 
@@ -88,14 +67,8 @@ export const ErrorWithPrimaryAction: Story = {
 	args: {
 		...Default.args,
 		title: 'This is the title',
-		primaryAction: { label: 'Primary action', onClick: console.log },
+		primaryAction: { label: 'Primary action', onClick: noop },
 		severity: 'error'
-	},
-	play: async ({ canvasElement }) => {
-		const canvas = within(canvasElement);
-		await expect(canvas.getByTestId(ICONS.errorBannerIcon)).toBeVisible();
-		await expect(canvas.getByText(/description is required/i)).toBeVisible();
-		await expect(canvas.getByRole('button', { name: /primary action/i })).toBeVisible();
 	}
 };
 
@@ -103,17 +76,10 @@ export const TypeOutline: Story = {
 	args: {
 		...Default.args,
 		title: 'This is the title',
-		primaryAction: { label: 'Primary action', onClick: console.log },
+		primaryAction: { label: 'Primary action', onClick: noop },
 		type: 'outline',
 		showClose: true,
 		onClose: () => noop
-	},
-	play: async ({ canvasElement }) => {
-		const canvas = within(canvasElement);
-		await expect(canvas.getByTestId(ICONS.successBannerIcon)).toBeVisible();
-		await expect(canvas.getByText(/description is required/i)).toBeVisible();
-		await expect(canvas.getByRole('button', { name: /primary action/i })).toBeVisible();
-		await expect(canvas.getByTestId(ICONS.close)).toBeVisible();
 	}
 };
 
@@ -121,52 +87,31 @@ export const SecondaryActionWithPrimaryAction: Story = {
 	args: {
 		...Default.args,
 		title: 'This is the title',
-		primaryAction: { label: 'Primary action', onClick: console.log },
-		secondaryAction: { label: 'Secondary action', onClick: console.log },
+		primaryAction: { label: 'Primary action', onClick: noop },
+		secondaryAction: { label: 'Secondary action', onClick: noop },
 		type: 'outline',
 		severity: 'warning',
 		showClose: true,
 		onClose: () => noop
-	},
-	play: async ({ canvasElement }) => {
-		const canvas = within(canvasElement);
-		await expect(canvas.getByTestId(ICONS.warningBannerIcon)).toBeVisible();
-		await expect(canvas.getByText(/description is required/i)).toBeVisible();
-		await expect(canvas.getByRole('button', { name: /primary action/i })).toBeVisible();
-		await expect(canvas.getByRole('button', { name: /secondary action/i })).toBeVisible();
-		await expect(canvas.getByTestId(ICONS.close)).toBeVisible();
 	}
 };
 
 export const OutlineWithInfoSeverity: Story = {
 	args: {
 		...Default.args,
-		primaryAction: { label: 'Primary action', onClick: console.log },
-		secondaryAction: { label: 'Secondary action', onClick: console.log },
+		primaryAction: { label: 'Primary action', onClick: noop },
+		secondaryAction: { label: 'Secondary action', onClick: noop },
 		type: 'outline',
 		severity: 'info'
-	},
-	play: async ({ canvasElement }) => {
-		const canvas = within(canvasElement);
-		await expect(canvas.getByTestId(ICONS.infoBannerIcon)).toBeVisible();
-		await expect(canvas.getByText(/description is required/i)).toBeVisible();
-		await expect(canvas.getByRole('button', { name: /primary action/i })).toBeVisible();
-		await expect(canvas.getByRole('button', { name: /secondary action/i })).toBeVisible();
 	}
 };
 
 export const OutlineWithErrorSeverity: Story = {
 	args: {
 		...Default.args,
-		primaryAction: { label: 'Primary action', onClick: console.log },
+		primaryAction: { label: 'Primary action', onClick: noop },
 		type: 'outline',
 		severity: 'error'
-	},
-	play: async ({ canvasElement }) => {
-		const canvas = within(canvasElement);
-		await expect(canvas.getByTestId(ICONS.errorBannerIcon)).toBeVisible();
-		await expect(canvas.getByText(/description is required/i)).toBeVisible();
-		await expect(canvas.getByRole('button', { name: /primary action/i })).toBeVisible();
 	}
 };
 
@@ -175,21 +120,10 @@ export const LongDescription: Story = {
 		...Default.args,
 		description:
 			'Text to edit: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry, Lorem Ipsum has been the industry',
-		primaryAction: { label: 'Primary action', onClick: console.log },
+		primaryAction: { label: 'Primary action', onClick: noop },
 		type: 'standard',
 		showClose: true,
 		onClose: () => noop
-	},
-	play: async ({ canvasElement }) => {
-		const canvas = within(canvasElement);
-		await expect(canvas.getByTestId(ICONS.successBannerIcon)).toBeVisible();
-		await expect(
-			canvas.getByText(
-				/Text to edit: Lorem Ipsum is simply dummy text of the printing and typesetting industry./i
-			)
-		).toBeVisible();
-		await expect(canvas.getByRole('button', { name: /primary action/i })).toBeVisible();
-		await expect(canvas.getByTestId(ICONS.close)).toBeVisible();
 	}
 };
 
@@ -199,25 +133,12 @@ export const ShortTitle: Story = {
 		title: 'Short title',
 		description:
 			'Text to edit: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry, Lorem Ipsum has been the industry',
-		primaryAction: { label: 'Primary action', onClick: console.log },
-		secondaryAction: { label: 'Secondary action', onClick: console.log },
+		primaryAction: { label: 'Primary action', onClick: noop },
+		secondaryAction: { label: 'Secondary action', onClick: noop },
 		type: 'standard',
 		severity: 'warning',
 		showClose: true,
 		onClose: () => noop
-	},
-	play: async ({ canvasElement }) => {
-		const canvas = within(canvasElement);
-		await expect(canvas.getByTestId(ICONS.warningBannerIcon)).toBeVisible();
-		await expect(canvas.getByText(/short title/i)).toBeVisible();
-		await expect(
-			canvas.getByText(
-				/Text to edit: Lorem Ipsum is simply dummy text of the printing and typesetting industry./i
-			)
-		).toBeVisible();
-		await expect(canvas.getByRole('button', { name: /primary action/i })).toBeVisible();
-		await expect(canvas.getByRole('button', { name: /secondary action/i })).toBeVisible();
-		await expect(canvas.getByTestId(ICONS.close)).toBeVisible();
 	}
 };
 
@@ -228,33 +149,12 @@ export const LongTitle: Story = {
 			'The "sixth sick sheik\'s sixth sheep\'s sick" is believed to be the toughest tongue twister in the English language.',
 		description:
 			'Text to edit: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry, Lorem Ipsum has been the industry',
-		primaryAction: { label: 'Primary action with long label', onClick: console.log },
-		secondaryAction: { label: 'Secondary action with long label', onClick: console.log },
+		primaryAction: { label: 'Primary action with long label', onClick: noop },
+		secondaryAction: { label: 'Secondary action with long label', onClick: noop },
 		type: 'standard',
 		severity: 'info',
 		showClose: true,
 		onClose: () => noop
-	},
-	play: async ({ canvasElement }) => {
-		const canvas = within(canvasElement);
-		await expect(canvas.getByTestId(ICONS.infoBannerIcon)).toBeVisible();
-		await expect(
-			canvas.getByText(
-				/The "sixth sick sheik's sixth sheep's sick" is believed to be the toughest tongue twister in the/i
-			)
-		).toBeVisible();
-		await expect(
-			canvas.getByText(
-				/Text to edit: Lorem Ipsum is simply dummy text of the printing and typesetting industry./i
-			)
-		).toBeVisible();
-		await expect(
-			canvas.getByRole('button', { name: /primary action with long label/i })
-		).toBeVisible();
-		await expect(
-			canvas.getByRole('button', { name: /secondary action with long label/i })
-		).toBeVisible();
-		await expect(canvas.getByTestId(ICONS.close)).toBeVisible();
 	}
 };
 
@@ -263,22 +163,11 @@ export const PrimaryActionWithIcon: Story = {
 		...Default.args,
 		title: 'Lorem ipsum dolor sit amet',
 		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-		primaryAction: { label: 'Primary action', onClick: console.log, icon: 'People' },
-		secondaryAction: { label: 'Secondary action', onClick: console.log },
+		primaryAction: { label: 'Primary action', onClick: noop, icon: 'People' },
+		secondaryAction: { label: 'Secondary action', onClick: noop },
 		type: 'standard',
 		severity: 'error',
 		showClose: true,
 		onClose: () => noop
-	},
-	play: async ({ canvasElement }) => {
-		const canvas = within(canvasElement);
-		await expect(canvas.getByTestId(ICONS.errorBannerIcon)).toBeVisible();
-		await expect(canvas.getByText('Lorem ipsum dolor sit amet')).toBeVisible();
-		await expect(
-			canvas.getByText(/Lorem ipsum dolor sit amet, consectetur adipiscing elit./i)
-		).toBeVisible();
-		await expect(canvas.getByRole('button', { name: /primary action/i })).toBeVisible();
-		await expect(canvas.getByTestId(ICONS.peopleIcon)).toBeVisible();
-		await expect(canvas.getByTestId(ICONS.close)).toBeVisible();
 	}
 };
