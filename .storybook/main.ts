5b433bcd487188eff7437449be54cbfa6530154e
Beatrice Guerra <beatrice.guerra@zextras.com>
2024-06-10 12:23:01 +0200

    docs: move table of icons to storybook
    Refs: CDS-245

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)', '../docs/**/*.mdx'],
 	addons: [
         '@storybook/addon-webpack5-compiler-babel',
         '@storybook/addon-essentials',
---
+++ docs/Icons.tsx
@@ -0,0 +1,87 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import React, { useCallback, useMemo, useState } from 'react';
+
+import {
+	Container,
+	Input,
+	ThemeProvider,
+	Icon,
+	Text,
+	InputProps,
+	Select,
+	SelectItem,
+	SingleSelectionOnChange
+} from '../src';
+import { Theme } from '../src/theme/theme';
+import { PaletteColor } from '../src/types/utils';
+
+const sortedIcons = Object.keys(Theme.icons).toSorted((a, b) => (a <= b ? -1 : 1));
+const paletteColors = Object.keys(Theme.palette).map(
+	(color) =>
+		({
+			label: color,
+			value: color as PaletteColor
+		}) satisfies SelectItem<PaletteColor>
+);
+export const Icons = (): React.JSX.Element => {
+	const [filter, setFilter] = useState('');
+	const [color, setColor] = useState<PaletteColor | undefined>();
+
+	const onInputChange = useCallback<NonNullable<InputProps['onChange']>>((e) => {
+		setFilter(e.target.value);
+	}, []);
+
+	const icons = useMemo(
+		() =>
+			sortedIcons.reduce<React.JSX.Element[]>((accumulator, icon) => {
+				if (filter.trim().length === 0 || icon.toLowerCase().toLowerCase().includes(filter)) {
+					accumulator.push(
+						<Container
+							key={icon}
+							orientation="vertical"
+							height={'fit'}
+							width={'25%'}
+							mainAlignment="center"
+							padding={{ vertical: 'medium', horizontal: 'extrasmall' }}
+							gap={'0.75rem'}
+							borderColor={'gray4'}
+						>
+							<Icon color={color} size="large" icon={icon} />
+							<Text size={'extrasmall'}>{icon}</Text>
+						</Container>
+					);
+				}
+				return accumulator;
+			}, []),
+		[color, filter]
+	);
+
+	const onColorChange = useCallback<SingleSelectionOnChange<PaletteColor>>((value) => {
+		setColor(value ?? undefined);
+	}, []);
+
+	return (
+		<ThemeProvider>
+			<Container gap={'0.5rem'} mainAlignment={'flex-start'}>
+				<Container orientation={'horizontal'} gap={'0.5rem'}>
+					<Input label="Filter" onChange={onInputChange} />
+					<Select items={paletteColors} onChange={onColorChange} label={'Color'} />
+				</Container>
+				<Container
+					orientation="horizontal"
+					height="fit"
+					width="fill"
+					mainAlignment={'flex-start'}
+					crossAlignment="flex-start"
+					wrap={'wrap'}
+				>
+					{icons}
+				</Container>
+			</Container>
+		</ThemeProvider>
+	);
+};
