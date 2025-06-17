8440155d48e6e34d9d95683c163919ade33f7971
Luca Stauble <luca.stauble@gmail.com>
2025-06-17 17:53:13 +0200

    refactor: rename module augmentation

--- src/types/emotion.ts
+++ src/types/emotion.ts
@@ -1,12 +1,12 @@
 /*
- * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
 import type { Theme } from '../theme/theme';
 
-declare module 'styled-components' {
+declare module '@emotion/react' {
 	// Augment DefaultTheme as suggested inside styled-components module
 	interface DefaultTheme extends Theme {}
 }
