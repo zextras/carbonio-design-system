d23fe869f44b0f4e8a9bb9bd21a12207db56d0c9
Luca Stauble <luca.stauble@gmail.com>
2025-06-23 12:09:07 +0200

    refactor: consider 'as' prop as transient

--- src/utils/emotion.ts
+++ src/utils/emotion.ts
@@ -6,5 +6,5 @@
 import { CreateStyled } from "@emotion/styled";
 
 export const transientOptions: Parameters<CreateStyled>[1] = {
-  shouldForwardProp: (propName: string) => !propName.startsWith('$'),
+  shouldForwardProp: (propName: string) => !propName.startsWith('$') && propName !== 'as',
 };
