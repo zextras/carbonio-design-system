1581ba7b5ad5b6ea2bdd9359df88541a55563293
Dennis <dennis.ceron@zextras.com>
2025-06-23 11:48:33 +0200

    refactor(layout): add transient props filter

--- src/components/layout/container/Container.tsx
+++ src/components/layout/container/Container.tsx
@@ -14,6 +14,7 @@ import { css } from '@emotion/react';
 import type { PaddingObj } from '../../../theme/theme-utils';
 import { getColor, getPadding } from '../../../theme/theme-utils';
 import type { AnyColor, LiteralUnion, With$Prefix } from '../../../types/utils';
+import { transientOptions } from '../../../utils/emotion';
 
 interface ContainerElProps {
 	/** The Container orientation (css flex-direction prop or 'vertical' or 'horizontal') */
@@ -98,7 +99,7 @@ interface ContainerElProps {
 	margin?: { left?: string; right?: string };
 }
 
-const ContainerEl = styled.div<With$Prefix<ContainerElProps>>`
+const ContainerEl = styled('div', transientOptions)<With$Prefix<ContainerElProps>>`
 	display: flex;
 	flex-direction: ${({ $orientation }): string | undefined => $orientation};
 	align-items: ${({ $crossAlignment }): string | undefined => $crossAlignment};
