55738b69b64c2dfb645d05313e4a6921111ad08d
Rodley <100950196+rodleyorosa@users.noreply.github.com>
2025-11-03 10:02:04 +0100

    fix: allow primary color on autoFocus
    Refs: CO-2645 (#532)

--- src/components/inputs/input/Input.tsx
+++ src/components/inputs/input/Input.tsx
@@ -136,7 +136,7 @@ const Input: Input = React.forwardRef<HTMLDivElement, InputProps>(function Input
 	},
 	ref
 ) {
-	const [hasFocus, setHasFocus] = useState(false);
+	const [hasFocus, setHasFocus] = useState(autoFocus);
 	const innerRef = useCombinedRefs<HTMLInputElement>(inputRef);
 	const [id] = useState(() => {
 		if (!Input._newId) {
