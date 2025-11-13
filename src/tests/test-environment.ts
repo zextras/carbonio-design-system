5bb8a69fbc2732558ee79f42315fe347fd18b762
Luca Stauble <luca.stauble@gmail.com>
2025-11-13 19:18:52 +0100

    refactor: add function to determine if running environment is test

---
+++ src/tests/test-environment.ts
@@ -0,0 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+export const isTestEnvironment: () => boolean = () => process.env.NODE_ENV === 'test';
